77c13e24f5d794e682d8b7f1e7024d3f9347b43a
sdmule <mulesaurabh45@gmail.com>
2026-07-09 15:03:43 +0530

    Code Cleanup

--- Program.cs
+++ Program.cs
@@ -20,60 +20,9 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 
-builder.Services.AddTransient<IMarketForecaster, MarketForecasterV2>();
-//builder.Services.AddTransient<IMarketForecaster, MarketForecaster>();
-//builder.Services.AddTransient<IMarketForecaster, MarketForecasterV2>();
-//builder.Services.AddSingleton<IMarketForecaster>(new MarketForecasterV2());//this can done while using only singleton as it will create one instance at starting and also we are using here "new" keyword
-
-////Use below way when we don't have abstraction
-//builder.Services.AddTransient<MarketForecasterV2>();
-//builder.Services.AddSingleton(new MarketForecasterV2());
-
-////Use below way when we have implementation only
-//builder.Services.AddTransient(typeof(MarketForecasterV2));
-//builder.Services.AddTransient(typeof(IMarketForecaster), typeof(MarketForecasterV2));
-
-builder.Services.AddTransient<TransientService>();
-builder.Services.AddScoped<ScopedService>();
-builder.Services.AddSingleton<SingletonService>();
-//TryAdd() - checks whether the implementation for this service is already exist and if exist then it will not register the new implementation
-//TryAdd() is an extension method provided by Microsoft.Extensions.DependencyInjection.Extensions that registers a service only if a service of that type has not already been registered.
-//builder.Services.TryAddTransient<IMarketForecaster, MarketForecaster>();
-
-//Below is the way to register same service multiple times
-//builder.Services.AddTransient<IMarketForecaster, MarketForecasterV2>();
-//builder.Services.AddTransient<IMarketForecaster, MarketForecaster>();
-
-//Below is the way to replace registration
-//builder.Services.AddTransient<IMarketForecaster, MarketForecasterV2>();
-//builder.Services.Replace(ServiceDescriptor.Transient<IMarketForecaster, MarketForecaster>());
-
-builder.Services.AddTransient<IMarketForecaster, MarketForecaster>();
-
-//RemoveAll<> - if we want to remove all the implemenatations    
-//builder.Services.AddTransient<IMarketForecaster, MarketForecasterV2>();
-//builder.Services.AddTransient<IMarketForecaster, MarketForecaster>();
-//builder.Services.RemoveAll<IMarketForecaster>();
-
-builder.Services.AddScoped<CreditApprovedHigh>();
-builder.Services.AddScoped<CreditApprovedLow>();
-builder.Services.AddScoped<Func<CreditApprovedEnum, ICreditApproved>>(ServiceProvider => range =>
-{
-    switch (range)
-    {
-        case CreditApprovedEnum.High: return ServiceProvider.GetService<CreditApprovedHigh>();
-        case CreditApprovedEnum.Low: return ServiceProvider.GetService<CreditApprovedLow>();
-        default: return ServiceProvider.GetService<CreditApprovedLow>();
-    }
-});
-
 builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
 
-builder.Services.AddAppSettingsConfig(builder.Configuration);
-
-builder.Services.AddScoped<IValidationChecker, AddressValidationChecker>();
-builder.Services.AddScoped<IValidationChecker, CreditValidationChecker>();
-builder.Services.AddScoped<ICreditValidator, CreditValidator>();
+builder.Services.AddAppSettingsConfig(builder.Configuration).AddAllServices();
 
 builder.Logging.AddFile("logs/creditApp-log-{Date}.txt");
 
@@ -93,7 +42,7 @@ else
 
 app.UseHttpsRedirection();
 app.UseRouting();
-
+app.UseAuthentication();
 app.UseAuthorization();
 app.UseMiddleware<CustomMiddleware>();
 app.MapStaticAssets();
---
+++ Utility/DI_Config/ConfigureDIServices.cs
@@ -0,0 +1,68 @@
+﻿using WazeCredit.Models;
+using WazeCredit.Service;
+using WazeCredit.Service.LifeTimeExample;
+using WazeCredit.Utility.AppSettingsClasses;
+
+namespace WazeCredit.Utility.DI_Config
+{
+
+    public static class ConfigureDIServices
+    {
+        public static IServiceCollection AddAllServices(this IServiceCollection services)
+        {
+            services.AddTransient<IMarketForecaster, MarketForecasterV2>();
+            //builder.Services.AddTransient<IMarketForecaster, MarketForecaster>();
+            //builder.Services.AddTransient<IMarketForecaster, MarketForecasterV2>();
+            //builder.Services.AddSingleton<IMarketForecaster>(new MarketForecasterV2());//this can done while using only singleton as it will create one instance at starting and also we are using here "new" keyword
+
+            ////Use below way when we don't have abstraction
+            //builder.Services.AddTransient<MarketForecasterV2>();
+            //builder.Services.AddSingleton(new MarketForecasterV2());
+
+            ////Use below way when we have implementation only
+            //builder.Services.AddTransient(typeof(MarketForecasterV2));
+            //builder.Services.AddTransient(typeof(IMarketForecaster), typeof(MarketForecasterV2));
+
+            services.AddTransient<TransientService>();
+            services.AddScoped<ScopedService>();
+            services.AddSingleton<SingletonService>();
+            //TryAdd() - checks whether the implementation for this service is already exist and if exist then it will not register the new implementation
+            //TryAdd() is an extension method provided by Microsoft.Extensions.DependencyInjection.Extensions that registers a service only if a service of that type has not already been registered.
+            //builder.Services.TryAddTransient<IMarketForecaster, MarketForecaster>();
+
+            //Below is the way to register same service multiple times
+            //builder.Services.AddTransient<IMarketForecaster, MarketForecasterV2>();
+            //builder.Services.AddTransient<IMarketForecaster, MarketForecaster>();
+
+            //Below is the way to replace registration
+            //builder.Services.AddTransient<IMarketForecaster, MarketForecasterV2>();
+            //builder.Services.Replace(ServiceDescriptor.Transient<IMarketForecaster, MarketForecaster>());
+
+            services.AddTransient<IMarketForecaster, MarketForecaster>();
+
+            //RemoveAll<> - if we want to remove all the implemenatations    
+            //builder.Services.AddTransient<IMarketForecaster, MarketForecasterV2>();
+            //builder.Services.AddTransient<IMarketForecaster, MarketForecaster>();
+            //builder.Services.RemoveAll<IMarketForecaster>();
+
+            services.AddScoped<CreditApprovedHigh>();
+            services.AddScoped<CreditApprovedLow>();
+            services.AddScoped<Func<CreditApprovedEnum, ICreditApproved>>(ServiceProvider => range =>
+            {
+                switch (range)
+                {
+                    case CreditApprovedEnum.High: return ServiceProvider.GetService<CreditApprovedHigh>();
+                    case CreditApprovedEnum.Low: return ServiceProvider.GetService<CreditApprovedLow>();
+                    default: return ServiceProvider.GetService<CreditApprovedLow>();
+                }
+            });
+
+            services.AddScoped<IValidationChecker, AddressValidationChecker>();
+            services.AddScoped<IValidationChecker, CreditValidationChecker>();
+            services.AddScoped<ICreditValidator, CreditValidator>();
+
+
+            return services;
+        }
+    }
+}
